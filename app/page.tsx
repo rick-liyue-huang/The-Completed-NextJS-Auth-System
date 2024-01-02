57cf22c27d7c5db4340afa44da501ffe17083351
Rick Huang <rick.liyue.huang@gmail.com>
2024-01-02 16:15:47 +1100

    feat: create auth login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,35 @@
+import { GoPasskeyFill } from 'react-icons/go';
+import { Podkova } from 'next/font/google';
+import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
+import { LoginButton } from '@/components/auth/login-button';
+
+const font = Podkova({
+  subsets: ['cyrillic-ext'],
+  weight: ['400', '700'],
+});
 
 export default function Home() {
+  console.log('Home');
   return (
-    <Button size="lg" variant={'custom'}>
-      Click me
-    </Button>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-blue-300 to-green-400">
+      <div className="space-y-6 text-center">
+        <h1
+          className={cn(
+            'text-6xl font-semibold text-white drop-shadow-md flex items-stretch justify-between',
+            font.className
+          )}
+        >
+          <GoPasskeyFill className="text-purple-500 mr-6" /> Authentication and
+          Authorization
+        </h1>
+        <p className="text-white text-lg">The Auth System</p>
+        <div>
+          <LoginButton>
+            <Button variant={'secondary'}>Log in</Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
